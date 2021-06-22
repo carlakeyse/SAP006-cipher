4b76f6016e48f52f2be3ce755ba268126d8e6118
Carla Nogueira <keyseprojectcontato@gmail.com>
2021-06-21 22:06:58 -0300

    Testes Ok e comentários retirados

--- src/cipher.js
+++ src/cipher.js
@@ -57,19 +57,6 @@ const cipher = {
     return msgDecripto;
   }
 
-  // A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
-  // 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
-
-  // (códigoDaletra + desloc) % tamDoAlfabeto
-  // (CodigoDaLetra + 7) % 26
-
-  // ((codigoDaLetraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
-
-  // exemplo (70 - 65 + 7) % 26 + 65
-
-  //                 -65         +desloc     %tam    +65
-  // codigoASC => codigo0a25 => desloco => giro => codigoASC
-
 };
 
 export default cipher;
